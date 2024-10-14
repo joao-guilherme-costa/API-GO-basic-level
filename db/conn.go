02df0107bea1cbb196afb9a604664b1c6df02e4b
joao-guilherme-costa <bilejg@gmail.com>
2024-10-13 23:02:16 -0300

    "just fixing some errors"

--- db/conn.go
+++ db/conn.go
@@ -17,7 +17,8 @@ const (
 
 func ConnectDB() (*sql.DB, error) {
 
-	psqlInfo := fmt.Sprintf("host =%s  port=%d user=%s"+ "password=%s dbname=%s sslmode=disable",
+	psqlInfo := fmt.Sprintf("host =%s  port=%d user=%s "+
+	 "password=%s dbname=%s sslmode=disable",
 	host, port, user, password, dbname)
 
 	db, err := sql.Open("postgres", psqlInfo)
